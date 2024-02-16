ec73f9955f377a33d06543ad7eeea3b317add2a9
Tim Weiss <tim.weiss10@web.de>
2024-02-16 14:15:42 +0000

    Add function for choosing grid size

--- run.py
+++ run.py
@@ -5,5 +5,19 @@ from random import randint
 print("Battleships Showdown")
 #User chooses their own name and will be adressed by it.  
 #Their board will also be referred to by the entered name.
-player_name = input("Please enter your name:")
+player_name = input("Please enter your name: ")
 print(player_name)
+#The user chooses the size of the board in a specified range.
+'''
+def board_size_battleships(number):
+    board_size_battleships(input("How big should the board grid be? Please choose between 4 and 8:"))
+    if input <= 3:
+        print("The chosen size is too small. Please enter a valid number.")
+    elif input >= 9:
+        print("The chosen size is too big. Please enter a valid number.")
+    else:
+        print("Thank you for choosing the board size. It will be", {board_size_battleships}, "x", {board_size_battleships})
+
+
+board_size_battleships()
+'''
