aeddfbae0f09e4a7c8aca3ecb45618b36c549b0c
Tim Weiss <tim.weiss10@web.de>
2024-02-16 12:36:19 +0000

    Add Input for player name

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
+#Import for generating random ship locations
+from random import randint
+
 #Headline for the game - stays always on screen
-print(Battleships Showdown)
+print("Battleships Showdown")
+#User chooses their own name and will be adressed by it.  
+#Their board will also be referred to by the entered name.
+player_name = input("Please enter your name:")
+print(player_name)
